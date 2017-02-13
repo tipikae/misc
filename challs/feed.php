3c341d75d55098fc98c6d60e04a40ec3147b56d8
tipikae <gilles.bd@free.fr>
2017-02-13 16:22:21 -0400

    Create feed.php

---
+++ challs/feed.php
@@ -0,0 +1,24 @@
+<rss version="2.0">
+	<channel>
+		<title>W3Challs</title>
+		<link>http://w3challs.com/</link>
+		<description>
+			World Wide Web Challenges : Hacking, Cracking, Programmation, Cryptographie et Stéganographie
+		</description>
+		<language>fr</language>
+		<copyright>Copyright 2008 - 2010 W3Challs</copyright>
+		<ttl>5</ttl>
+		<item>
+			<title>
+				<![CDATA[ Nouveaux challenges Web ]]>
+			</title>
+			<link>http://www.w3challs.com/news/75</link>
+			<description>
+				<![CDATA[
+<?php echo 'prout'."\r\n";?>
+				]]>
+			</description>
+			<pubDate>le 12/05/2016 à 19h36</pubDate>
+		</item>
+	</channel>
+</rss>
