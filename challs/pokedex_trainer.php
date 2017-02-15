023f58de916bf45ea005dd03f2279b19a748a5d4
tipikae <gilles.bd@free.fr>
2017-02-15 09:09:11 -0400

    Update pokedex_trainer.php

--- challs/pokedex_trainer.php
+++ challs/pokedex_trainer.php
@@ -1,1 +1,22 @@
-php://filter/convert.base64-encode/resource=index.php
+<rss version="2.0">
+	<channel>
+		<title>W3Challs</title>
+		<link>http://w3challs.com/</link>
+		<description>
+			World Wide Web Challenges : Hacking, Cracking, Programmation, Cryptographie et Stéganographie
+		</description>
+		<language>fr</language>
+		<copyright>Copyright 2008 - 2010 W3Challs</copyright>
+		<ttl>5</ttl>
+		<item>
+			<title>
+				Nouveaux challenges Web
+			</title>
+			<link>http://www.w3challs.com/news/75</link>
+			<description>
+				<?php echo 'prout';?>
+			</description>
+			<pubDate>le 12/05/2016 à 19h36</pubDate>
+		</item>
+	</channel>
+</rss>
